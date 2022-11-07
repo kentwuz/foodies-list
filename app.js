710064df1c85d131fb8be469fec2a177c63609e1
0xKent <kentw8888@gmail.com>
2022-11-07 10:30:33 +0800

    feat: render index page with json file
    add restaurant json file
    set node server status
    trim the index-page code and render the restaurant list with a loop

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log(hello)
+//basic setting
+const express = require('express')
+const app = express()
+const port = 3000
+//loading json file
+const restaurantList = require('./restaurant.json')
+//express handlebars
+const { engine } = require('express-handlebars')
+//setting template engine
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+//setting static file
+app.use(express.static('public'))
+//routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurant: restaurantList.results })
+})
+
+app.listen(port, () => {
+  console.log('the server is running')
+})
